348600934bc59697b4f0bd82dc156ba21f45fe6a
FellynnIckwr <fellixfnf@gmail.com>
2024-01-12 23:38:50 +0700

    old Psych FPS Counter

--- songs/PsychUI.hx
+++ songs/PsychUI.hx
@@ -9,6 +9,7 @@ import flixel.math.FlxPoint;
 import openfl.events.KeyboardEvent;
 import openfl.display.DisplayObject;
 import openfl.display.Sprite;
+import openfl.display.FPS;
 import openfl.text.TextField;
 import openfl.text.TextFormat;
 import openfl.ui.Keyboard;
@@ -19,16 +20,9 @@ import openfl.text.TextFormat;
 import openfl.Lib;
 import flixel.FlxG;
 import funkin.options.Options;
+import funkin.backend.system.Main;
 
-// fps vars
-var finalFPS:Float = 0;
-var memories = Math.abs(FlxMath.roundDecimal(System.totalMemory / 1000000, 1));
-var camFPS = null;
-var fpsfunniCounter:FlxText;
-
-var cacheCount:Int;
-var currentTime:Float;
-var times:Array<Float>;
+var fpsfunniCounter:FPS;
 
 public var colouredBar = (dad != null && dad.xml != null && dad.xml.exists("color")) ? CoolUtil.getColorFromDynamic(dad.xml.get("color")) : 0xFFFFFFFF;
 public var sicks:Int = 0;
@@ -72,14 +66,11 @@ function onPostNoteCreation(event) {
 }
 
 function getRating(accuracy:Float):String {
-    if (accuracy < 0) {
-        return "?";
-    }
-    for (rating in ratingStuff) {
-        if (accuracy < rating[1]) {
-            return rating[0];
-        }
-    }
+    if (accuracy < 0) return "?";
+
+    for (rating in ratingStuff)
+        if (accuracy < rating[1]) return rating[0];
+
     return ratingStuff[ratingStuff.length - 1][0];
 }
 
@@ -119,9 +110,8 @@ function create() {
     timeBar = new FlxBar(timeBarBG.x + 4, timeBarBG.y + 4, FlxBar.FILL_LEFT_TO_RIGHT, Std.int(timeBarBG.width - 8), Std.int(timeBarBG.height - 8), Conductor, 'songPosition', 0, 1);
     timeBar.scrollFactor.set();
     timeBar.createFilledBar(0xFF000000,0xFFFFFFFF);
-    if (FlxG.save.data.colouredBar) {
-        timeBar.createFilledBar(0xFF000000, colouredBar);
-    }
+    if (FlxG.save.data.colouredBar) timeBar.createFilledBar(0xFF000000, colouredBar);
+
     timeBar.numDivisions = 400;
     timeBar.alpha = 0;
     timeBar.value = Conductor.songPosition / Conductor.songDuration;
@@ -139,46 +129,33 @@ function create() {
     timeTxt.cameras = [camHUD];
     PauseSubState.script = 'data/scripts/funnypause';
 
-    FlxG.cameras.add(camFPS = new HudCamera(), false);
-    camFPS.bgColor = 0;
-    fpsfunniCounter = new FlxText(10,10, 400, 18);
-    fpsfunniCounter.setFormat("_sans", 14, FlxColor.WHITE, "LEFT");
-    fpsfunniCounter.antialiasing = false;
-    fpsfunniCounter.scrollFactor.set();
-    fpsfunniCounter.cameras = [camFPS];
-    add(fpsfunniCounter);
-    cacheCount = 0;
-    currentTime = 0;
-    times = [];
-    finalFPS = 0;
     Framerate.fpsCounter.visible = false;
     Framerate.memoryCounter.visible = false;
     Framerate.codenameBuildField.visible = false;
+
+    fpsfunniCounter = new FPS(10, 3);
+    fpsfunniCounter.defaultTextFormat = new TextFormat("_sans", 14, 0xFFFFFF);
+    Main.instance.addChild(fpsfunniCounter);
 }
 
 function onSongStart() {
-    if (timeBar != null) {
+    if (timeBar != null)
         FlxTween.tween(timeBar, {alpha: 1}, 0.5, {ease: FlxEase.circOut});
-    }
-    if (timeBarBG != null) {
-        FlxTween.tween(timeBarBG, {alpha: 1}, 0.5, {ease: FlxEase.circOut});
-    }
-    if (timeTxt != null) {
-    FlxTween.tween(timeTxt, {alpha: 1}, 0.5, {ease: FlxEase.circOut});
-    }
-    if (botplayTxt != null && FlxG.save.data.botplayOption) {
-        FlxTween.tween(botplayTxt, {alpha: 1}, 0.5, {ease: FlxEase.circOut});
-    }
-}
 
-function postUpdate(elapsed:Float) {
-    
+    if (timeBarBG != null)
+        FlxTween.tween(timeBarBG, {alpha: 1}, 0.5, {ease: FlxEase.circOut});
+
+    if (timeTxt != null)
+        FlxTween.tween(timeTxt, {alpha: 1}, 0.5, {ease: FlxEase.circOut});
+
+    if (botplayTxt != null && FlxG.save.data.botplayOption)
+        FlxTween.tween(botplayTxt, {alpha: 1}, 0.5, {ease: FlxEase.circOut});
 }
 
 function update(elapsed:Float) {
-    if (inst != null && timeBar != null && timeBar.max != inst.length) {
+    if (inst != null && timeBar != null && timeBar.max != inst.length)
         timeBar.setRange(0, Math.max(1, inst.length));
-    }
+
     if (inst != null && timeTxt != null) {
         var timeRemaining = Std.int((inst.length - Conductor.songPosition) / 1000);
         var seconds = CoolUtil.addZeros(Std.string(timeRemaining % 60), 2);
@@ -206,35 +183,28 @@ function update(elapsed:Float) {
 
     var acc = FlxMath.roundDecimal(Math.max(accuracy, 0) * 100, 2);
     var rating:String = getRating(accuracy);
-    if (songScore > 0 || acc > 0 || misses > 0) {
+    if (songScore > 0 || acc > 0 || misses > 0)
         hudTxt.text = "Score: " + songScore + " | Misses: " + misses +  " | Rating: " + rating + " (" + acc + "%)" + " - " + ratingFC;
-    }
+
     if (FlxG.save.data.botplayOption) {
         botplaySine += 180 *  FlxG.elapsed;
         botplayTxt.alpha = 1 - Math.sin((Math.PI * botplaySine) / 180);
         player.cpu = true;
     }
-    finalFPS = CoolUtil.fpsLerp(finalFPS, FlxG.elapsed == 0 ? 0 : (1 / FlxG.elapsed), 0.25);
-    fpsfunniCounter.text = "FPS: " + Std.string(Math.floor(finalFPS)) + "\nMemory: " + memories + " MB";
-    if (memories == 3000 || finalFPS <= FlxG.save.data.Framerate / 2) {
-        fpsfunniCounter.color = FlxColor.RED;
-    }
 }
 
 function beatHit() {
     psychIconP1.scale.set(1.2, 1.2);
-    psychIconP1.updateHitbox();
-
     psychIconP2.scale.set(1.2, 1.2);
+
+    psychIconP1.updateHitbox();
     psychIconP2.updateHitbox();
 }
 
 function onPlayerHit(event) {
     if (event.note.isSustainNote) return;
 
-    if(hudTxtTween != null) {
-        hudTxtTween.cancel();
-    }
+    if(hudTxtTween != null) hudTxtTween.cancel();
     hudTxt.scale.x = 1.075;
     hudTxt.scale.y = 1.075;
     hudTxtTween = FlxTween.tween(hudTxt.scale, {x: 1, y: 1}, 0.2, {onComplete: function(twn:FlxTween) {hudTxtTween = null;}});
@@ -250,21 +220,16 @@ function onPlayerHit(event) {
 		if (bads > 0 || shits > 0) ratingFC = 'FC';
 		else if (goods > 0) ratingFC = 'GFC';
 		else if (sicks > 0) ratingFC = 'SFC';
-	}
-	else if (misses < 10) ratingFC = 'SDCB';
+	} else if (misses < 10) ratingFC = 'SDCB';
 }
 
 function postCreate() {
-    for (i in [missesTxt, accuracyTxt, scoreTxt]) {
-        i.visible = false;
-    }
-
-    if (downscroll) {
-        hudTxt.y = healthBarBG.y - 58;
-    }
+    for (i in [missesTxt, accuracyTxt, scoreTxt]) i.visible = false;
+    if (downscroll) hudTxt.y = healthBarBG.y - 58;
 
     healthBar.y = FlxG.height * 0.89;
     healthBarBG.y = healthBar.y - 4;
+
     iconP1.y = healthBar.y - 75;
     iconP2.y = iconP1.y;
 
@@ -283,19 +248,9 @@ function postCreate() {
 
     add(hudTxt);
 
-    if (!downscroll) {
-        hudTxt.y = healthBarBG.y + 38;
-    }
-
-    if (FlxG.save.data.showBar) {
-        for (i in [timeTxt, timeBar, timeBarBG]) {
-            i.visible = false;
-        }
-    }
-
-    if (FlxG.save.data.showTxt) {
-        hudTxt.visible = false;
-    }
+    if (!downscroll) hudTxt.y = healthBarBG.y + 38;
+    if (FlxG.save.data.showBar) for (i in [timeTxt, timeBar, timeBarBG]) i.visible = false;
+    if (FlxG.save.data.showTxt) hudTxt.visible = false;
 }
 
 function makePsychIcon(icon:FlxSprite, char:String, isPlayer:Bool) {
@@ -322,4 +277,6 @@ function destroy() {
     Framerate.fpsCounter.visible = true;
     Framerate.memoryCounter.visible = true;
     Framerate.codenameBuildField.visible = true;
+
+    Main.instance.removeChild(fpsfunniCounter);
 }
