a91e5e0ee9c6e38fd6a9e117eb65850e048d5ac4
Koi <145818807+koixc@users.noreply.github.com>
2024-02-25 23:23:30 -0500

    Did Some "Cool Stuff"

--- data/notes/GF Sing.hx
+++ data/notes/GF Sing.hx
@@ -1,5 +1,8 @@
-function onNoteHit(_)
-    if (_.noteType == "GF Sing") {
-        _.cancelAnim();
-        gf.playSingAnim(_.direction, _.animSuffix, 'SING', _.forceAnim);
+function onNoteHit(e){
+    if (e.noteType == "GF Note"){
+        e.cancelAnim();
+        gf.playSingAnim(e.direction, e.animSuffix);
+        // you can also do strumLines.members[2].characters[0].playSingAnim
+        // very useful if you are making multiple people sing independently, requires multiple versions of this notetype though!
     }
+}
